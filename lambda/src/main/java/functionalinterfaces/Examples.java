5db39d1a797b45a8a3a2644a18951198cb0799ee
Agageldi Beghanov <agabegh@gmail.com>
2022-09-03 14:03:19 +0200

    functional interface

--- lambda/src/main/java/functionalinterfaces/Examples.java
+++ lambda/src/main/java/functionalinterfaces/Examples.java
@@ -1,6 +1,6 @@
 package functionalinterfaces;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Examples {
     public static void main(String[] args) {
@@ -16,5 +16,23 @@ public class Examples {
         Predicate<Integer> lesserThan = a -> a < 18; //Implementation of test method that belongs to Predicate interface
         Boolean result = lesserThan.test(50);
         System.out.println(result);
+
+        //************CONSUMER************//
+        Consumer<Integer> display = i -> System.out.println(i);
+        display.accept(50);
+
+        //************BICONSUMER************//
+        BiConsumer<Integer, Integer> addTwo = (x,y) -> System.out.println(x+y);
+        addTwo.accept(300,200);
+
+        //************FUNCTION************//
+        Function<String,String> fun = s -> "Hello " + s;
+        String str = fun.apply("Aga");
+        System.out.println(str);
+
+        //************SUPPLIER************//
+        Supplier<Double> randomValue = () -> Math.random();
+        System.out.println(randomValue.get());
+
     }
 }

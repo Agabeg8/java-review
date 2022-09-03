6b6ecbe01ae18a17289872ec46bea547166e3877
Agageldi Beghanov <agabegh@gmail.com>
2022-09-03 14:38:45 +0200

    functional interface

--- lambda/src/main/java/AppleTest.java
+++ lambda/src/main/java/AppleTest.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class AppleTest {
 
@@ -12,15 +13,31 @@ public class AppleTest {
         inventory.add(new Apple(200,Color.GREEN));
         inventory.add(new Apple(50,Color.RED));
 
-        List<Apple> heavyApple = filterApples(inventory, new AppleHeavyPredicate());
-        System.out.println(heavyApple);
+//        List<Apple> heavyApple = filterApples(inventory, new AppleHeavyPredicate());
+//        System.out.println(heavyApple);
+//
+//        List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
+//        System.out.println(greenApple);
 
-        List<Apple> greenApple = filterApples(inventory, new AppleGreenColorPredicate());
+
+        List<Apple> greenApple = filterApples(inventory, apple -> apple.getColor().equals(Color.GREEN));
         System.out.println(greenApple);
 
     }
 
-    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate) {
+//    private static List<Apple> filterApples(List<Apple> inventory, ApplePredicate applePredicate) {
+//
+//        List<Apple> result = new ArrayList<>();
+//
+//        for (Apple apple : inventory){
+//            if (applePredicate.test(apple)){
+//                result.add(apple);
+//            }
+//        }
+//        return result;
+//    }
+
+    private static List<Apple> filterApples(List<Apple> inventory, Predicate<Apple> applePredicate) {
 
         List<Apple> result = new ArrayList<>();
 

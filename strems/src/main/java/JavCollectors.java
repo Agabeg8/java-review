6e293bf416f2473542e440e771121dd4118b9d0e
Agageldi Beghanov <agabegh@gmail.com>
2022-09-15 21:47:00 +0200

    Java Collectors

--- strems/src/main/java/JavCollectors.java
+++ strems/src/main/java/JavCollectors.java
@@ -8,7 +8,7 @@ public class JavCollectors {
         List<Integer> numbers = Arrays.asList(3,4,5,6,6,7);
 
         //toCollection(Supplier): is used to create a collection using collector
-        System.out.println("=======TOCOLLOCTION======");
+        System.out.println("========TOCOLLOCTION=======");
         List<Integer> numberList = numbers.stream()
                 .filter(x -> x%2 == 0)
                 .collect(Collectors.toCollection(ArrayList::new));
@@ -22,7 +22,7 @@ public class JavCollectors {
 
         //toList() : return a CCollector interface that gathers the input data into a new list
 
-        System.out.println("=======TOLIST======");
+        System.out.println("========TOLIST=======");
         List<Integer> numberList2 = numbers.stream()
                 .filter(x -> x%2 == 0)
                 .collect(Collectors.toList());
@@ -30,7 +30,7 @@ public class JavCollectors {
         System.out.println(numberList2);
 
         //toSet : return a Collector interface that gathers the input data into a new set
-        System.out.println("=======TOSET======");
+        System.out.println("========TOSET=======");
         Set<Integer> numberSet2 = numbers.stream()
                 .filter(x -> x%2 == 0)
                 .collect(Collectors.toSet());
@@ -38,11 +38,24 @@ public class JavCollectors {
         System.out.println(numberSet2);
 
         //toMap(Function, Function) : return a Collector interface that gathers the input data into a new map
-        System.out.println("=======TOMAP======");
+        System.out.println("========TOMAP=======");
         Map<String, Integer> dishMap = DishData.getAll().stream()
                 .collect(Collectors.toMap(Dish::getName, Dish::getCalories));
 
         System.out.println(dishMap);
+
+        //counting()  : returns a Collector that counts the number of the elements
+        System.out.println("========COUNTING=======");
+        Long evenCount = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.counting());
+
+        System.out.println(evenCount);
+
+        //summingInt(ToIntFunction) : returns a Collector that produces the sum of a integer-valued func
+        Integer sum = DishData.getAll().stream()
+                .collect(Collectors.summingInt(Dish::getCalories));
+        System.out.println(sum);
 
     }
 }

e0b485502e6925791cdddfc1452cfb3ae2b554cb
Agageldi Beghanov <agabegh@gmail.com>
2022-09-15 20:48:57 +0200

    Java Collectors

---
+++ strems/src/main/java/JavCollectors.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class JavCollectors {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(3,4,5,6,6,7);
+
+        //toCollection(Supplier): is used to create a collection using collector
+        System.out.println("=======TOCOLLOCTION======");
+        List<Integer> numberList = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        System.out.println(numberList);
+
+        Set<Integer> numberSet = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.toCollection(HashSet::new));
+        System.out.println(numberSet);
+
+        //toList() : return a CCollector interface that gathers the input data into a new list
+
+        System.out.println("=======TOLIST======");
+        List<Integer> numberList2 = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(numberList2);
+
+        //toSet : return a Collector interface that gathers the input data into a new set
+        System.out.println("=======TOSET======");
+        Set<Integer> numberSet2 = numbers.stream()
+                .filter(x -> x%2 == 0)
+                .collect(Collectors.toSet());
+
+        System.out.println(numberSet2);
+
+        //toMap(Function, Function) : return a Collector interface that gathers the input data into a new map
+        System.out.println("=======TOMAP======");
+        Map<String, Integer> dishMap = DishData.getAll().stream()
+                .collect(Collectors.toMap(Dish::getName, Dish::getCalories));
+
+        System.out.println(dishMap);
+
+    }
+}
